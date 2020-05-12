09dac2c979f95ec2f18a6a679adcb07f0b1d0396
alex <geniofuturo@gmail.com>
2020-05-12 14:38:01 -0300

    Allow custom map.

--- htmlentities/__init__.py
+++ htmlentities/__init__.py
@@ -11,21 +11,27 @@ import re
 __version__ = '0.3.0'
 
 
-def encode(source):
+def encode(source, mcodepoint2name=None):
     new_source = ''
+
+    if mcodepoint2name is None:
+        mcodepoint2name = codepoint2name
 
     for char in source:
         val = ord(char)
-        if val in codepoint2name:
-            char = '&%s;' % codepoint2name[val]
+        if val in mcodepoint2name:
+            char = '&%s;' % mcodepoint2name[val]
         new_source += char
 
     return new_source
 
 
-def decode(source):
+def decode(source, name2codepointm=None):
+    if name2codepointm is None:
+        name2codepointm = name2codepoint
+
     for entitie in re.findall('&(?:[a-z][a-z0-9]+);', source, re.I):
         entitie = entitie.replace('&', '')
         entitie = entitie.replace(';', '')
-        source = source.replace('&%s;' % entitie, unichr(name2codepoint[entitie]))
+        source = source.replace('&%s;' % entitie, unichr(name2codepointm[entitie]))
     return source
